c72f28f9e55c60920f242c26f02983a2e37b25d4
Jordan Hemsworth <jordanhemsworth@gmail.com>
2021-07-27 14:46:19 +0100

    Display the list with CSS selections

--- TourOfHeroes/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
+  
+  heroes = HEROES;  //Set heroes as HEROES array
+
+  selectedHero?: Hero;  //Set hero type.
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
+
 
   hero: Hero = {    // hero property is Type Hero.
     id: 1,
     name: 'Windstorm'
-  };
+  };  
 
-
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
